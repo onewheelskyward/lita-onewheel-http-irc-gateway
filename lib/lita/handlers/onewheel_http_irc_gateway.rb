6a4f2b21e1a4284016c973150d1b27a8c1b99c2d
Andrew Kreps <andrew.kreps@gmail.com>
2015-10-15 15:18:42 -0700

    Works

--- lib/lita/handlers/onewheel_http_irc_gateway.rb
+++ lib/lita/handlers/onewheel_http_irc_gateway.rb
@@ -1,12 +1,15 @@
 module Lita
   module Handlers
     class OnewheelHttpIrcGateway < Handler
+      # extend HTTPRouter
       # insert handler code here
-      http.get '/yo', :yo
+      http.post '/yo', :yo
 
       def yo(request, response)
+        response.body << request.params['text']
         robot = request.env['lita.robot']
-        robot.send_message('#pdxbots', )
+        source = Lita::Source.new(user: nil, room: '#pdxbots')
+        robot.send_messages(source, request.params['text'][0..100])
       end
 
       Lita.register_handler(self)
